8bdd3b95890f674082b3451219c31490a79803c8
Florent Valette <florent.valette@gmail.com>
2025-12-15 11:18:10 +0000

    features, dtschema: fix autogenerated test
    do not check version in autogenerated test as it will be broken
    at each new release.
    
    TODO: add test scenarii w/ differente package version installed.

--- test/dtschema/test.sh
+++ test/dtschema/test.sh
@@ -3,5 +3,5 @@
 set -e
 
 source dev-container-features-test-lib
-check "check dtschema version" bash -c "dt-validate --version | grep '2025.8'"
+check "check dtschema version" bash -c "dt-validate --version"
 reportResults
